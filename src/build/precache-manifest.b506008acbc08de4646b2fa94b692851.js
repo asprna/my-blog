9632068584f051d9463f79b1f46078a7e5c582cd
asprna <asankaprabath@yahoo.co.uk>
2019-06-15 15:51:03 +1000

    First commit

---
+++ src/build/precache-manifest.b506008acbc08de4646b2fa94b692851.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97004a9315edbb50144ad34171a9ebf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3879c8c961b33fb11ab0",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "65525d98e6835d505d2f",
+    "url": "/static/js/2.f5e6359f.chunk.js"
+  },
+  {
+    "revision": "3879c8c961b33fb11ab0",
+    "url": "/static/js/main.c91b53e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
